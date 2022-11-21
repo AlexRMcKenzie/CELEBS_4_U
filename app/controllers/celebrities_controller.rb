6fd9dc452035c91cccf5e21947a1d1ed853d6337
Gabriel Wilkinson <gabriel.wilkinson@hotmail.com>
2022-11-21 18:07:01 +0000

    created index show pages and links between 2 for celebrities

---
+++ app/controllers/celebrities_controller.rb
@@ -0,0 +1,11 @@
+class CelebritiesController < ApplicationController
+
+  def index
+    @celebrities = Celebrity.all
+  end
+
+  def show
+    @celebrity = Celebrity.find(params[:id])
+  end
+
+end
