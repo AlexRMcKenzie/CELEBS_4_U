6a4c11b3a8af85e3abeb8c57130ceb3b03abcd4c
George Tsagiannis <gtsagiannis@gmail.com>
2022-11-22 17:15:11 +0000

    Basic functionality for making a booking. Create booking if dates available

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    root to: "pages#home"
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-
-    resources :celebrities, only: [:index, :show] do
-      resources :bookings, only: [:new, :create]
+    resources :celebrities, only: %i[index show] do
+      resources :bookings, only: %i[new create]
     end
+    resources :bookings, only: %i[show index]
 
     # resources :bookings, only: [:show, :edit, :update, :destroy]
-
 
   # Defines the root path route ("/")
   # root "articles#index"
